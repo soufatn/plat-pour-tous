03140281933e938a4d7b7ad322f774582dd57c75
Sofiane Abdallah <soufatn@msn.com>
2023-08-28 11:55:05 +0200

    add firebase config

---
+++ plat-pour-tous-front/src/app/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBrYCIfSoRCGKNw8quW00tE567i3t_0LVw",
+    authDomain: "plat-pour-tous.firebaseapp.com",
+    projectId: "plat-pour-tous",
+    storageBucket: "plat-pour-tous.appspot.com",
+    messagingSenderId: "247653182925",
+    appId: "1:247653182925:web:48776dafc3ee8d633f6884",
+    measurementId: "G-0EJP390QE8"
+  }
+};
